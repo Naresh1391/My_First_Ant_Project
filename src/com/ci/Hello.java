f3b41732c04c52a1ae74cd1096d4fad828c519ee
mucha <mucha@DESKTOP-HA1TEQ7>
2016-04-05 00:43:20 +0530

    initial commit

---
+++ src/com/ci/Hello.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.ci;
+
+/**
+ * @author mucha
+ *
+ */
+public class Hello {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello...!!");
+	}
+
+}
